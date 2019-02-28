8a5a3b7e329a169157ac41290d7833d95e96ef41
Kim Pham <pham.kim.a@gmail.com>
2019-02-27 18:21:50 -0600

    landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './cover.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+          <header className="masthead mb-auto">
+            <div className="inner">
+              <h3 className="masthead-brand">Shifting Gears</h3>
+              <nav className="nav nav-masthead justify-content-center">
+                <div className="nav-link active">About</div>
+                <div className="nav-link">Login</div>
+                <div className="nav-link">Sign up</div>
+              </nav>
+            </div>
+          </header>
+
+          <main role="main" className="inner cover">
+            <h1 className="cover-heading">Instructions</h1>
+            <p className="lead cover-lead">1.</p>
+            <p className="lead cover-lead">2.</p>
+            <div className="btn btn-lg btn-secondary">Play</div>
+          </main>
+
+          <footer className="mastfoot mt-auto">
+            <div className="inner">
+             <p>Photo by Vlad Smolyakov on Unsplash</p>
+            </div>
+          </footer>
+        </div>
+
       </div>
     );
   }

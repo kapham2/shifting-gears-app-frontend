5e3633b977cf43a0da0be46affb6a6463b675bdc
Kim Pham <pham.kim.a@gmail.com>
2019-02-28 15:52:16 -0600

    update active state on click nav

--- src/components/CoverContainer.js
+++ src/components/CoverContainer.js
@@ -2,8 +2,27 @@ import React from 'react'
 import '../cover.css'
 import SigninForm from './SigninForm'
 
-class Cover extends React.Component {
+class CoverContainer extends React.Component {
+
+  state = {
+    active: "Sign in"
+  }
+
+  onClickUpdateActive = (e) => {
+    const newActive = e.target
+    const oldActive = document.getElementsByClassName("nav-link active")[0]
+    
+    if (newActive !== oldActive) {
+      oldActive.classList.remove("active")
+      newActive.classList.add("active")
+      this.setState({
+          active: e.target.getAttribute("name")
+      })
+    }
+  }
+
   render() {
+    // console.log("CoverContainer: this.state => ", this.state)
     return (
       <div className="cover-container d-flex w-100 h-100 p-2 mx-auto flex-column">
         <header className="masthead mb-auto">
@@ -12,17 +31,16 @@ class Cover extends React.Component {
               <img className="img-icon" src="/icon.png" alt="" /> Shifting Gears
             </h4>
             <nav className="nav nav-masthead justify-content-center">
-              <div className="nav-link active">Sign in</div>
-              <div className="nav-link">Sign up</div>
-              <div className="nav-link">Incognito</div>
-
+              <div className="nav-link active" name="Sign in" onClick={this.onClickUpdateActive} >Sign in</div>
+              <div className="nav-link" name="Sign up" onClick={this.onClickUpdateActive}>Sign up</div>
+              <div className="nav-link" name="Use Incognito Mode" onClick={this.onClickUpdateActive}>Incognito</div>
             </nav>
           </div>
         </header>
 
         <div className="d-flex justify-content-center">
           <main role="main" className="inner cover">
-            <SigninForm />
+            <SigninForm active={this.state.active} />
           </main>
         </div>
 
@@ -36,4 +54,4 @@ class Cover extends React.Component {
   }
 }
 
-export default Cover
+export default CoverContainer
--- src/components/SigninForm.js
+++ src/components/SigninForm.js
@@ -2,14 +2,26 @@ import React from 'react'
 import '../signin.css'
 
 class SigninForm extends React.Component {
+
+  onSubmitForm = (e) => {
+    e.preventDefault()
+  }
+
   render() {
+    // console.log("SigninForm: this.props => ", this.props)
+    const input = this.props.active === "Use Incognito Mode" 
+      ? "" 
+      : (
+        <div>
+          <input type="text" id="inputUsername" className="form-control" placeholder="Username" required autoFocus />
+          <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+        </div>
+        )
+
     return(
-      <form className="form-signin">
-        <h5 className="mb-4">Please sign in</h5>
-        <input type="text" id="inputUsername" className="form-control" placeholder="Username" required autoFocus />
-        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-        <button className="btn btn-primary shadow-sm btn-block shadow-sm" type="submit">Sign in</button>
-        <p className="mt-4 mb-3">Don't have an account? Sign up</p>
+      <form className="form-signin" onSubmit={this.onSubmitForm}>
+        { input }
+        <button className="btn shadow-sm btn-block" type="submit">{this.props.active}</button>
       </form>
     )
   }

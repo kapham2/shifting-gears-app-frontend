974e8bd7b959d7439b965e6a5b5baf01e95b2bd8
Kim Pham <pham.kim.a@gmail.com>
2019-02-27 21:17:28 -0600

    update icon & styling

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ class App extends Component {
       <div className="App">
 
         <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-          <header className="masthead mb-auto">
+          <header className="masthead mb-auto shadow-sm rounded">
             <div className="inner">
-              <h3 className="masthead-brand">Shifting Gears</h3>
+              <h3 className="masthead-brand">
+                <img className="img-icon" src="/icon.png" alt="" />
+                Shifting Gears
+              </h3>
               <nav className="nav nav-masthead justify-content-center">
                 <div className="nav-link active">About</div>
                 <div className="nav-link">Login</div>
@@ -20,7 +23,7 @@ class App extends Component {
             </div>
           </header>
 
-          <main role="main" className="inner cover">
+          <main role="main" className="inner cover shadow-sm rounded">
             <h1 className="cover-heading">Instructions</h1>
             <p className="lead cover-lead">1.</p>
             <p className="lead cover-lead">2.</p>
@@ -29,7 +32,7 @@ class App extends Component {
 
           <footer className="mastfoot mt-auto">
             <div className="inner">
-             <p>Photo by Vlad Smolyakov on Unsplash</p>
+             <p>App by Kim Pham</p>
             </div>
           </footer>
         </div>

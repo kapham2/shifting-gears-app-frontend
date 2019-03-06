87a259592f4eadda806334f17603932c1d829be4
Kim Pham <pham.kim.a@gmail.com>
2019-03-06 14:22:30 -0600

    Updates

---
+++ precache-manifest.ddd170617d13ba577762f3877f5c0474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b085999f717b1797b61",
+    "url": "/shifting-gears-app-frontend/static/css/main.d8564c34.chunk.css"
+  },
+  {
+    "revision": "9b085999f717b1797b61",
+    "url": "/shifting-gears-app-frontend/static/js/main.9b085999.chunk.js"
+  },
+  {
+    "revision": "cf8fbdf5c87498653c18",
+    "url": "/shifting-gears-app-frontend/static/js/runtime~main.cf8fbdf5.js"
+  },
+  {
+    "revision": "d41b95adddafa4d20e15",
+    "url": "/shifting-gears-app-frontend/static/css/2.9385564d.chunk.css"
+  },
+  {
+    "revision": "d41b95adddafa4d20e15",
+    "url": "/shifting-gears-app-frontend/static/js/2.d41b95ad.chunk.js"
+  },
+  {
+    "revision": "676302934b658d02ec98a149e61cb412",
+    "url": "/shifting-gears-app-frontend/index.html"
+  }
+];

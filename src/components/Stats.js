da9e3f560df30c05293b03e42b832849bf75df4c
Kim Pham <pham.kim.a@gmail.com>
2019-03-21 10:10:50 -0500

    update score display

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -75,7 +75,7 @@ class Stats extends React.Component {
                   <tr key={stat.id}>
                     <th scope="row">{rank}</th>
                     <td>{stat.user.username}</td>
-                    <td>{Math.round(stat.avg_velocity * 100) / 100}</td>
+                    <td>{Math.round(stat.avg_velocity * 1000) / 1000}</td>
                   </tr>
                 )
               })
